fe3ad62e402dc3b49e1cefc39e18d152c28c987b
Seunghyun Kim <qwazqwert@gmail.com>
2020-04-23 16:05:37 -0400

    Add files via upload
    Uses Serial Plotter to display pulse data (PulseSensor Playground EX)

---
+++ PulseSensor_SP/PulseSensor_SP.ino
@@ -0,0 +1,49 @@
+#define USE_ARDUINO_INTERRUPTS true
+#include <PulseSensorPlayground.h>
+
+const int OUTPUT_TYPE = SERIAL_PLOTTER; // Outputs to Serial Plotter
+
+const int PULSE_INPUT = A0;
+const int PULSE_BLINK = 13;    // Pin 13 is the on-board LED
+const int PULSE_FADE = 5;
+const int THRESHOLD = 550;   // Adjust this number to avoid noise when idle and to count bpm
+
+PulseSensorPlayground pulseSensor;
+void setup() {
+  /*
+     Use 115200 baud because that's what the Processing Sketch expects to read,
+     and because that speed provides about 11 bytes per millisecond.
+
+     If we used a slower baud rate, we'd likely write bytes faster than
+     they can be transmitted, which would mess up the timing
+     of readSensor() calls, which would make the pulse measurement
+     not work properly.
+  */
+  Serial.begin(115200);
+
+  pulseSensor.analogInput(PULSE_INPUT);
+  pulseSensor.blinkOnPulse(PULSE_BLINK);
+  pulseSensor.fadeOnPulse(PULSE_FADE);
+
+  pulseSensor.setSerial(Serial);
+  pulseSensor.setOutputType(OUTPUT_TYPE);
+  pulseSensor.setThreshold(THRESHOLD);
+
+  if (!pulseSensor.begin()) {
+
+    for(;;) {
+      digitalWrite(PULSE_BLINK, LOW);
+      delay(50);
+      digitalWrite(PULSE_BLINK, HIGH);
+      delay(50);
+    }
+  }
+}
+
+void loop() {
+  delay(20);
+ pulseSensor.outputSample();
+  if (pulseSensor.sawStartOfBeat()) {
+   pulseSensor.outputBeat();
+  }
+}
